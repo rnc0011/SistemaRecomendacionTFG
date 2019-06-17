e648c4379b7e24cc17d16205fcb777053b3c5930
Raúl Negro Carpintero <rnc0011@alu.ubu.es>
2019-06-17 20:15:38 +0200

    Interfaz para construir los modelos hecha #70
    Se crean todas las páginas relacionadas con la creación de modelos de recomendación.

--- src/vista/Flask.py
+++ src/vista/Flask.py
@@ -7,7 +7,7 @@ Created on Tue Apr 20 17:02:09 2019
 
 # Importo todo lo necesario
 from flask import Flask, render_template, request, redirect, url_for
-from Forms import HomeForm, DatasetForm, NuevoDatasetForm, DatasetsPruebaForm
+from Forms import *
 
 app = Flask(__name__)
 app.secret_key = 'development key'
@@ -24,28 +24,79 @@ def home():
 	return render_template('home.html', titulo='Página Principal', form=form)
 
 
-@app.route("/home/construir_modelo", methods=['GET','POST'])
+@app.route("/home/construir_modelo", methods=['GET', 'POST'])
 def construir_modelo():
 	form = DatasetForm(request.form)
-	if form.menu.data == '1':
-		return redirect(url_for('nuevo_dataset'))
+	if request.method == 'GET':
+		return render_template('construir_modelo.html', titulo='Construir Modelo', form=form)
 	else:
-		return redirect(url_for('datasets_prueba'))
-	return render_template('construir_modelo.html', titulo='Construir Modelo', form=form)
+		if form.menu.data == '1':
+			return redirect(url_for('nuevo_dataset'))
+		else:
+			return redirect(url_for('datasets_prueba'))
 
 
 @app.route("/home/construir_modelo/nuevo_dataset", methods=['GET','POST'])
 def nuevo_dataset():
 	form = NuevoDatasetForm(request.form)
-	print(request.form['encoding'], request.form['separador'])
-	return render_template('nuevo_dataset.html', methods=['GET','POST'], form=form)
+	if request.method == 'GET':
+		return render_template('nuevo_dataset.html', titulo='Nuevo Dataset', form=form)
+	else:
+		print(request.form['encoding'], request.form['separador'])
+		# obtener los dataframes
+		return redirect(url_for('elegir_modelo'))
 
 
 @app.route("/home/construir_modelo/datasets_prueba", methods=['GET','POST'])
 def datasets_prueba():
 	form = DatasetsPruebaForm(request.form)
-	return render_template('datasets_prueba.html', titulo='Cargar Dataset', form=form)
+	if request.method == 'GET':
+		return render_template('datasets_prueba.html', titulo='Datasets Prueba', form=form)
+	else:
+		# Lo que sea
+		return redirect(url_for('elegir_modelo'))
 
+
+@app.route("/home/construir_modelo/elegir_modelo", methods=['GET', 'POST'])
+def elegir_modelo():
+	form = ElegirModeloForm(request.form)
+	if request.method == 'GET':
+		return render_template('elegir_modelo.html', titulo='Elegir Modelo', form=form)
+	else:
+		if form.menu.data == '1':
+			return redirect(url_for('elegir_modelo_clasico'))
+		else:
+			return redirect(url_for('elegir_modelo_dl'))
+
+
+@app.route("/home/construir_modelo/elegir_modelo/elegir_modelo_clasico", methods=['GET', 'POST'])
+def elegir_modelo_clasico():
+	form = ElegirModeloClasicoForm(request.form)
+	if request.method == 'GET':
+		return render_template('elegir_modelo_clasico.html', titulo='Modelos Clásicos', form=form)
+	else:
+		return redirect(url_for('param_clasico'))
+
+
+@app.route("/home/construir_modelo/elegir_modelo/elegir_modelo_dl", methods=['GET', 'POST'])
+def elegir_modelo_dl():
+	form = ElegirModeloDLForm(request.form)
+	if request.method == 'GET':
+		return render_template('elegir_modelo_dl.html', titulo='Modelos Deep Learning', form=form)
+	else:
+		return redirect(url_for('param_dl'))
+
+
+@app.route("/home/construir_modelo/elegir_modelo/elegir_modelo_clasico/param_clasico", methods=['GET', 'POST'])
+def param_clasico():
+	form = ParamClasicoForm(request.form)
+	return render_template('param_clasico.html', titulo='Parámetros Modelo Clásico', form=form)
+
+
+@app.route("/home/construir_modelo/elegir_modelo/elegir_modelo_dl/param_dl", methods=['GET', 'POST'])
+def param_dl():
+	form = ParamDLForm(request.form)
+	return render_template('param_dl.html', titulo='Parámetros Modelo Deep Learning', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- src/vista/Forms.py
+++ src/vista/Forms.py
@@ -1,15 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import RadioField, SubmitField, SelectField, MultipleFileField
+from wtforms import RadioField, SubmitField, SelectField, MultipleFileField, IntegerField, FloatField
 
 class HomeForm(FlaskForm):
 	choices = [(1, 'Construir modelo de recomendación'), (2, 'Cargar modelo'), (3, 'Añadir valoraciones a conjunto existente')]
 	menu = RadioField(choices=choices)
 	submit = SubmitField('Siguiente')
 
+
 class DatasetForm(FlaskForm):
 	choices = [(1, 'Con un nuevo dataset'), (2, 'Con datasets de prueba')]
 	menu = RadioField(choices=choices)
 	submit = SubmitField('Siguiente')
+
 
 class NuevoDatasetForm(FlaskForm):
 	encoding = SelectField(choices=[('utf-8', 'utf-8'), ('ISO-8859-1', 'ISO-8859-1')])
@@ -17,10 +19,55 @@ class NuevoDatasetForm(FlaskForm):
 	archivos = MultipleFileField('Archivos .csv')
 	submit = SubmitField('Siguiente')
 
+
 class DatasetsPruebaForm(FlaskForm):
 	choices = [(1, 'Movielens'), (2, 'Anime'), (3, 'Book Crossing'), (4, 'Last FM'), (5, 'Dating Agency'), (6, 'Otro')]
 	menu = RadioField(choices=choices)
 	submit = SubmitField('Siguiente')
 
+
+class ElegirModeloForm(FlaskForm):
+	choices = [(1, 'Modelo Clásico'), (2, 'Modelo basado en Aprendizaje Profundo')]
+	menu = RadioField(choices=choices)
+	submit = SubmitField('Siguiente')
+
+
+class ElegirModeloClasicoForm(FlaskForm):
+	choices = [(1, 'Modelo Colaborativo'), (2, 'Modelo Híbrido'), (3, 'Modelo por Contenido')]
+	menu = RadioField(choices=choices)
+	submit = SubmitField('Siguiente')
+
+
+class ElegirModeloDLForm(FlaskForm):
+	choices = [(1, 'Modelo de Factorización Explícito'), (2, 'Modelo de Factorización Implícito'), (3, 'Modelo de Secuencia Implícito')]
+	menu = RadioField(choices=choices)
+	submit = SubmitField('Siguiente')
+
+
+class ParamClasicoForm(FlaskForm):
+	no_components = IntegerField(default=10)
+	k = IntegerField(default=5)
+	n = IntegerField(default=10)
+	learning_schedule = SelectField(default='adagrad', choices=[('adagrad', 'adagrad'), ('adadelta', 'adadelta')])
+	loss = SelectField(default='logistic',choices=[('logistic', 'logistic'), ('bpr', 'BPR'), ('warp', 'WARP'), ('warp-kos', 'k-OS WARP')])
+	learning_rate = FloatField(default=0.05)
+	rho = FloatField(default=0.95)
+	epsilon = FloatField(default=1e-06)
+	item_alpha = FloatField(default=0.0)
+	user_alpha = FloatField(default=0.0)
+	max_sampled = IntegerField(default=10)
+	epochs = IntegerField(default=30)
+	submit = SubmitField('Construir')
+
+
+class ParamDLForm(FlaskForm):
+	loss = SelectField(choices=[('regression', 'Regression (Fact. Explícito)'), ('poisson', 'Poisson (Fact. Explícito)'), ('logistic', 'logistic (Fact. Explícito)'), ('pointwise', 'Pointwise (Fact. Implícito y Secuencia)'), ('bpr', 'BPR (Fact. Implícito y Secuencia)'), ('hinge', 'Hinge (Fact. Implícito y Secuencia)'), ('adaptive hinge', 'Adaptive Hinge (Fact. Implícito y Secuencia)')])
+	embedding_dim = IntegerField(default=32)
+	n_iter = IntegerField(default=10)
+	batch_size = IntegerField(default=256)
+	l2 = FloatField(default=0.0)
+	learning_rate = FloatField(default=0.01)
+	epochs = IntegerField(default=30)
+	submit = SubmitField('Construir')
 
 
